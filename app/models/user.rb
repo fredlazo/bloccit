f900203917ebd3d5be0bf81c8d8fe324af6ffc60
Frederick Lazo <fredlazo@yahoo.com>
2016-05-30 12:23:07 -0700

    Checkpoint 40 completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,20 @@
 class User < ActiveRecord::Base
-  has_many :posts  
-  before_save { self.email = email.downcase if email.present? }
+  has_many :posts
 
+  before_save { self.email = email.downcase if email.present? }
+  before_save { self.role ||= :member }
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
-
- validates :password, presence: true, length: { minimum: 6 }, unless: :password_digest
- validates :password, length: { minimum: 6 }, allow_blank: true
 
   validates :email,
             presence: true,
             uniqueness: { case_sensitive: false },
             length: { minimum: 3, maximum: 254 }
+  validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
+  validates :password, length: { minimum: 6 }, allow_blank: true
 
 
   has_secure_password
+  enum role: [:member, :admin]
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160530141345) do
+ActiveRecord::Schema.define(version: 20160530182101) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20160530141345) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "role"
   end
 
 end
