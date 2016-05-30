a8604c8d8070be74b64924ceeaa80a5e7de94397
Frederick Lazo <fredlazo@yahoo.com>
2016-05-30 14:59:40 -0700

    Checkpoint 42 completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
     @post.user = current_user
 # #10
     if @post.save
-# #11
+      @post.labels = Label.update_labels(params[:post][:labels])
       flash[:notice] = "Post was saved successfully."
       redirect_to [@topic, @post]
     else
@@ -40,6 +40,7 @@ class PostsController < ApplicationController
 
 
     if @post.save
+      @post.labels = Label.update_labels(params[:post][:labels])
       flash[:notice] = "Post was updated successfully."
       redirect_to [@post.topic, @post]
     else
@@ -76,5 +77,5 @@ class PostsController < ApplicationController
        redirect_to [post.topic, post]
      end
    end
-      
+
 end
