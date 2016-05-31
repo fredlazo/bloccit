4be2d8191051cd2049a8bf5b58c8efe4b0b91b1d
Frederick Lazo <fredlazo@yahoo.com>
2016-05-30 17:08:06 -0700

    Assignment 43 completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,25 +5,25 @@ class Post < ActiveRecord::Base
    has_many :votes, dependent: :destroy
    has_many :labelings, as: :labelable
    has_many :labels, through: :labelings
+
+   after_create :create_vote
+
    default_scope { order('rank DESC') }
-   
+
    validates :title, length: { minimum: 5 }, presence: true
    validates :body, length: { minimum: 20 }, presence: true
    validates :topic, presence: true
    validates :user, presence: true
 
     def up_votes
-  # #9
       votes.where(value: 1).count
     end
 
     def down_votes
-  # #10
       votes.where(value: -1).count
     end
 
     def points
-  # #11
       votes.sum(:value)
     end
 
@@ -33,4 +33,9 @@ class Post < ActiveRecord::Base
       update_attribute(:rank, new_rank)
     end
 
+    private
+
+    def create_vote
+      user.votes.create(value: 1, post: self)
+    end
 end
